456fe6a9117e06a2d8f449cd31bced854d14cc50
IjzerenHein <hrutjes@gmail.com>
2015-09-09 09:25:49 +0200

    Command-line interface

--- src/rtfToHtml.js
+++ src/rtfToHtml.js
@@ -2,53 +2,62 @@ import program from 'commander';
 import fs from 'fs';
 import parser from './parser/parser';
 import htmlFormatter from './formatters/htmlFormatter';
+import packageJson from '../package.json';
 
 program
-  .version('0.0.1')
-  .usage('[options] file [outputFile]')
-  .option('-f, --format [format]', 'Output format [format]', 'html')
-  .option('-m, --margins [top, right, left, bottom]', 'Margins')
-  .option('-w, --width [width]', 'Width')
-  .option('-h, --height [height]', 'Height')
-  .option('-va, --vertalign [align]', 'Vertical alignment', 'top/center/bottom')
+  .version(packageJson.version)
+  .usage('[options] rtfFile htmlFile')
+  //.option('-f, --format [format]', 'Output format [format]', 'html')
+  .option('-m, --margins [top,right,left,bottom]', 'Override margins (twips)')
+  .option('-w, --width [width]', 'Override paperwidth (twips)')
+  .option('-h, --height [height]', 'Override paperheight (twips)')
+  .option('-va, --vertalign [align]', '"top", "center", "bottom"')
   .parse(process.argv);
 
 console.log('rtfToHtml v' + program.version());
-console.log('Converting "' + program.args[0] + '" -> "' + program.args[1] + '"');
-const options = {
-    margins: [0, 0, 0, 0],
-    vertAlign: 'top'
-};
-if (program.width) {
-    options.width = program.width;
-    console.log('--width ' + program.width);
+const source = program.args[0];
+const dest = program.args[1];
+if (!source || !dest) {
+    console.log('\nno source-file or destination-file specified');
+    program.outputHelp();
 }
-if (program.height) {
-    options.height = program.height;
-    console.log('--height ' + program.height);
-}
-if (program.margins) {
-    options.margins = JSON.parse(program.margins);
-    console.log('--margins ' + program.margins);
-}
-if (program.vertalign) {
-    options.vertAlign = program.vertalign;
-    console.log('--vertalign ' + program.vertalign);
-}
+else {
+    console.log('Converting "' + source + '" -> "' + dest + '"');
+    const options = {
+        margins: [0, 0, 0, 0],
+        vertAlign: 'top'
+    };
+    if (program.width) {
+        options.width = program.width;
+        console.log('--width ' + program.width);
+    }
+    if (program.height) {
+        options.height = program.height;
+        console.log('--height ' + program.height);
+    }
+    if (program.margins) {
+        options.margins = JSON.parse(program.margins);
+        console.log('--margins ' + program.margins);
+    }
+    if (program.vertalign) {
+        options.vertAlign = program.vertalign;
+        console.log('--vertalign ' + program.vertalign);
+    }
 
-fs.readFile(program.args[0], 'utf8', (err, data) => {
-    if (err) {
-        return console.error(err);
-    }
-    const parsedRtf = parser.parse(data);
-    console.log(JSON.stringify(parsedRtf, undefined, 2));
-    const formatter = htmlFormatter;
-    const output = formatter(parsedRtf, options);
-    if (program.args.length >= 2) {
-        fs.writeFile(program.args[1], output, (err2) => {
-            if (err2) {
-                console.log('ERROR: ' + err2);
-            }
-        });
-    }
-});
+    fs.readFile(source, 'utf8', (err, data) => {
+        if (err) {
+            return console.error(err);
+        }
+        const parsedRtf = parser.parse(data);
+        console.log(JSON.stringify(parsedRtf, undefined, 2));
+        const formatter = htmlFormatter;
+        const output = formatter(parsedRtf, options);
+        if (program.args.length >= 2) {
+            fs.writeFile(dest, output, (err2) => {
+                if (err2) {
+                    console.log('ERROR: ' + err2);
+                }
+            });
+        }
+    });
+}
